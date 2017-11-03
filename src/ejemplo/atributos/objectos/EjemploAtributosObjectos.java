7c9d651059c855a7da28cf5c9df79fa5677a7aaa
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-03 09:00:05 +0100

    modiC

--- src/ejemplo/atributos/objectos/EjemploAtributosObjectos.java
+++ src/ejemplo/atributos/objectos/EjemploAtributosObjectos.java
@@ -6,11 +6,12 @@ public class EjemploAtributosObjectos {
 
     public static void main(String[] args) {
        Conta conta=new Conta("millonetis","100",1000);
-      
+        System.out.println(conta.toString());
         System.out.println(conta.getSaldoTotal());
         //System.out.println(Conta.getXuro());
         //System.out.println(conta.saldo);
         System.out.println("xuro = "+Conta.getXuro());
     }
+    //Terminado
     
 }
